7dad85ac0217f110c59b2084877dc9ed6a37e1a6
hiloki <service@inkdesign.jp>
2019-12-06 00:46:26 +0900

    log: inline-css

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const moment = require("moment");
 const localImages = require("eleventy-plugin-local-images");
+const CleanCSS = require("clean-css");
 
 moment.locale("ja");
 
@@ -10,6 +11,10 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter("dateReadable", date => {
     return moment(date).format("LL"); // E.g. May 31, 2019
+  });
+
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
   });
 
   eleventyConfig.addPlugin(localImages, {
---
+++ log/inline-css.md
@@ -0,0 +1,13 @@
+---
+layout: layouts/log.njk
+title: インラインでCSSを突っ込む
+date: 2019-12-05
+tags: ["log"]
+---
+
+<time datetime="{{ post.date | dateIso }}">{{ post.date | dateReadable }}</time>
+
+# {{ title }}
+
+[公式の Tips](https://www.11ty.dev/docs/quicktips/inline-css/)を参考に、単純な CSS を仮でいれてみた。
+試しただけなので後で書き換える。
